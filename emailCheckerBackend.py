fb03c4695312923ee99710a2489c45be7332c1b4
Benjamin <lochmann.b@gmail.com>
2019-04-27 23:02:59 +0200

    comments added to backend and template

--- emailCheckerBackend.py
+++ emailCheckerBackend.py
@@ -4,13 +4,20 @@ from check_email_address_function import check_email_address
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+'''
+Handles GET and POST requests to /.
+A get request will be invoked by the browser when browsing to /
+A post request will be invoked by the browser when the user submits the form with the email address
+'''
+@app.route('/', methods=['GET', 'POST']) 
 def index():
     email = ''
     check_result = ''
 
+    #check if an email address has been submitted. If so, check the email address vor syntactic correctness
     if request.method == "POST" and "email" in request.form:
         email = request.form["email"]
         check_result = check_email_address(email)
 
+    #render index.html
     return render_template('index.html', email=email, check_result=check_result)
