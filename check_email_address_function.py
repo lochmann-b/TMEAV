d5b9360b3b629e7c7c09c2cf4845a0171de9fc47
lisakaser <kaser.lisa@gmail.com>
2019-04-29 11:10:47 +0200

    changed regex

--- check_email_address_function.py
+++ check_email_address_function.py
@@ -4,7 +4,8 @@ def check_email_address(email_address):
     to ensure the user did not enter something else than an email address by mistake"""
     
     import re
-    regexp = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    #regexp = re.compile(r"[^@]+@[^@]+\.[^@]+") This was a first test regex not very good
+    regexp = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
     if regexp.search(email_address):
         return 'This is likely an email address'
     else:
