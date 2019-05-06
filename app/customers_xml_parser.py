8c440a93978031f7b3a498f58d766381dbc81da9
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-05-06 16:58:17 +0200

    intermediate commit

--- app/customers_xml_parser.py
+++ app/customers_xml_parser.py
@@ -1,53 +1,18 @@
-from xml.sax import make_parser, handler
-from  app.email_utils import are_email_addresses_valid
+from xml.sax import make_parser
+from app.email_utils import are_email_addresses_valid
+from app.CustomerHandler import CustomerHandler
 
-class CustomerHandler(handler.ContentHandler):
+def is_invalid_line(current_name, current_email):
+    #incomplete lines are considered as valid
+    if current_name and current_email:
+        return not are_email_addresses_valid(current_email)
+    return False
 
-    def __init__(self):
-        self.invalid_lines = [] #used to collect the invalid lines
-        self.current_column = -1 #current column beeing parsed. 0 = name, 1 = email address, -1 = none
-        self.current_name = '' # name found in current line
-        self.current_email = '' # email address found in current line
-
-    def startElement(self, name, attrs):
-        if name == 'Row':
-            # we are at the start of a row.
-            # reset row related stuff
-            self.current_column = -1
-            self.current_name = ''
-            self.current_email = ''
-        elif name == 'Cell':
-            # we are at the start of a column.
-            # increase column index
-            self.current_column += 1
-
-    def characters(self, content):
-        # we now are extracting content found between a a start tag and an end tag
-        # please note that this method could be called multiple times for the same start/end tag.
-        # Therefore, the content as to be appended to the current name/email address
-
-        if self.current_column == 0:
-            # append content of first column to current_name
-            self.current_name += content.strip()
-        elif self.current_column == 1:
-            # append content of second column to current_email
-            self.current_email += content.strip()
-
-    def is_valid_line(self):
-        #incomplete lines are considered as valid
-        if self.current_name and self.current_email:
-           return are_email_addresses_valid(self.current_email)
-        return True
-
-    def endElement(self, name):
-        # we hit the end of a row.
-        # check if the collected name / email address should be considered as invalid
-        if name == "Row":
-         if not self.is_valid_line():
-            self.invalid_lines.append((self.current_name, self.current_email))
-
-    def get_invalid_lines(self):
-        return self.invalid_lines
+def is_valid_line(current_name, current_email):
+    #incomplete lines are considered as valid
+    if current_name and current_email:
+        return are_email_addresses_valid(current_email)
+    return False
 
 def check_email_addresses(file):
     """Scans the xml file for invalid email addresses.
@@ -64,7 +29,20 @@ def check_email_addresses(file):
         a list of tuples representing the lines where invalid email addreses have been found.        
     """
     parser = make_parser()
-    cust_handler = CustomerHandler()
+    cust_handler = CustomerHandler(lambda name, e_mail: is_invalid_line(name, e_mail))
     parser.setContentHandler(cust_handler)
     parser.parse(file)
     return cust_handler.get_invalid_lines()
+
+    def chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+def split_email_addresses(file, chunk_size):
+    parser = make_parser()
+    cust_handler = CustomerHandler(lambda name, e_mail: is_valid_line(name, e_mail))
+    parser.setContentHandler(cust_handler)
+    parser.parse(file)
+    
+    l = cust_handler.get_invalid_lines()
+    return [r";".join(map(lambda x: x[1], l[i:i + chunk_size])) for i in range(0, len(l), chunk_size)]
