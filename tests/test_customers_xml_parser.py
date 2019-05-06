f8f005229f3f2345fb6a3d2979478742cd953c8e
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-05-06 14:23:26 +0200

    test for customer xml parser implemented

---
+++ tests/test_customers_xml_parser.py
@@ -0,0 +1,16 @@
+#Note to run the test, type "python -m unittest tests/test_customers_xml_parser.py" in the terminal window
+
+import unittest
+
+from app.customers_xml_parser import check_email_addresses
+
+
+class TestCostomersXMLParser(unittest.TestCase):
+    def test_check_email_addresses(self):
+        file_path = "tests/sampleCustomers.xml"
+        result = check_email_addresses(open(file_path, "r"))
+        print(result)
+        self.assertEqual(result,  [('Name', 'email'), ('Arthur Dent', '42@betelgeuse')])
+
+if __name__ == '__main__':
+    unittest.main()
