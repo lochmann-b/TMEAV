8bf0ed81ea3184ba38eeccc45340ff68ca115583
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-05-06 14:36:38 +0200

    tests for semicolon delimited addresses added

--- tests/test_is_email_address_valid.py
+++ tests/test_is_email_address_valid.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from app.email_utils import is_email_address_valid
+from app.email_utils import is_email_address_valid, are_email_addresses_valid
 
 
 class TestEmailChecker(unittest.TestCase):
@@ -47,6 +47,48 @@ class TestEmailChecker(unittest.TestCase):
         data = "firstname.lastname@domain.edu"
         result = is_email_address_valid(data)
         self.assertEqual(result, True)
+
+    def test_email_addresses_separated_by_semicolon_should_pass(self):
+        """
+        Test semicolon separated emailsare TRUE
+        """
+        data = "max@test.org;magnus@carlsen.com"
+        result = are_email_addresses_valid(data)
+        self.assertEqual(result, True)
+
+    def test_incomplete_email_addresses_separated_by_semicolon_should_fail(self):
+        """
+        Test incomplete email addresses separated are FALSE
+        """
+        data = "max@test.org;magnus@carlsen"
+        result = are_email_addresses_valid(data)
+        self.assertEqual(result, False)
+
+    def test_email_address_with_trailing_semicolon_should_fail(self):
+        """
+        Test email addresse with trailing seimicolon is FALSE
+        """
+        data = "max@test.org;"
+        result = are_email_addresses_valid(data)
+        self.assertEqual(result, False)
+    
+    def test_email_address_with_leading_semicolon_should_fail(self):
+        """
+        Test email addresse with leading  seimicolon is FALSE
+        """
+        data = ";max@test.org"
+        result = are_email_addresses_valid(data)
+        self.assertEqual(result, False)
+    
+    def test_single_email_address_should_pass(self):
+        """
+        Single email address is TRUE
+        """
+        data = "max@test.org"
+        result = are_email_addresses_valid(data)
+        self.assertEqual(result, True)
+
+
 
 if __name__ == '__main__':
     unittest.main()
