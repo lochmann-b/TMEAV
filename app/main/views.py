83dc2145823927263898be5f83774b0ecf8df3ab
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-05-17 11:09:53 +0200

    imports for api fixed

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, url_for
+from flask import render_template, session, url_for, request, abort, jsonify
 from flask_login import login_required
 from . import main
 from .. import db
